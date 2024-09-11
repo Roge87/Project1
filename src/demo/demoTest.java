fca85b144e0c9b1c3cff125d0109d29f974dacf2
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-11 12:15:08 -0400

    prueba

--- src/demo/demoTest.java
+++ src/demo/demoTest.java
@@ -1,5 +1,13 @@
 package demo;
 
 public class demoTest {
+	
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public void rest(int a, int b) {
+		System.out.println(a - b);
+	}
 
 }
