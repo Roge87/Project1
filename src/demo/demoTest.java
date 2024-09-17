da9e15f9ee5676ce62712bac1e847dd66fbe352a
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-17 14:49:22 -0400

    testNew

--- src/demo/demoTest.java
+++ src/demo/demoTest.java
@@ -1,7 +1,7 @@
 package demo;
 
 public class demoTest {
-	
+
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
@@ -10,13 +10,16 @@ public class demoTest {
 		System.out.println(a - b);
 	}
 
-	public void div(int a, int b){
-		System.out.println(a/b);
+	public void div(int a, int b) {
+		System.out.println(a / b);
 	}
 
 	public void promedio(int a, int b) {
 		int result = a * b / 2;
 		System.out.println(result);
 	}
-	
+
+	public void newTest() {
+
+	}
 }
